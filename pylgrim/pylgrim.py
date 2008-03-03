6e33554db597340300099672eb9f3e9c55c92759
josch <josch@46df4e5c-bc4e-4628-a0fc-830ba316316d>
2008-03-03 14:12:38 +0000

    added gpl
    git-svn-id: http://www.neo1973-germany.de/svn@12 46df4e5c-bc4e-4628-a0fc-830ba316316d

--- pylgrim/pylgrim.py
+++ pylgrim/pylgrim.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python
 #coding=utf8
 
+'''
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 WIDTH = 480
 HEIGHT = 640
 
@@ -45,7 +60,7 @@ class TestView(edje.Edje):
     def download(self, x,y,z):
         import urllib
         try:
-            webFile = urllib.urlopen("http://a.tile.openstrzzeetmap.org/%d/%d/%d.png"%(z,x,y))
+            webFile = urllib.urlopen("http://a.tile.openstreetmap.org/%d/%d/%d.png"%(z,x,y))
             if not os.path.exists("%d"%z):
                 os.mkdir("%d"%z)
             if not os.path.exists("%d/%d"%(z,x)):
