c9d612d5c7c00c499088c8b007a258052ec81640
PatrickBeck <PatrickBeck@46df4e5c-bc4e-4628-a0fc-830ba316316d>
2008-09-11 19:09:30 +0000

    modified
    git-svn-id: http://www.neo1973-germany.de/svn@146 46df4e5c-bc4e-4628-a0fc-830ba316316d

--- unitc/install.sh
+++ unitc/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir /usr/share/unitc
-cp unitc.xml /usr/share/unitc
+cp unit.xml /usr/share/unitc
 chmod +x unit_converter.py
 cp unit_converter.py /usr/bin/unitc
 cp unitc.desktop /usr/share/applications/
