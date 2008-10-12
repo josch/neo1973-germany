dcdec41a4c5d1fa9c1262b861738a1edd3fba6ff
PatrickBeck <PatrickBeck@46df4e5c-bc4e-4628-a0fc-830ba316316d>
2008-10-12 18:45:55 +0000

    Corrected the filenames
    git-svn-id: http://www.neo1973-germany.de/svn@189 46df4e5c-bc4e-4628-a0fc-830ba316316d

--- unitc/install.sh
+++ unitc/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir /usr/share/unitc
 cp unit.xml /usr/share/unitc
-chmod +x unit_converter.py
-cp unit_converter.py /usr/bin/unitc
+chmod +x unitc.py
+cp unitc.py /usr/bin/unitc
 cp unitc.desktop /usr/share/applications/
